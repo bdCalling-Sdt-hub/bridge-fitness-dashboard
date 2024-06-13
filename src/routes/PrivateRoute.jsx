b50724a08312f9d8c8466f18c4be424851e3b8f5
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-06-13 18:06:21 +0600

    privet route

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,38 +1,18 @@
-// import React from "react";
-// import { Navigate, useLocation } from "react-router-dom";
-
-// const PrivateRoute = ({ children }) => {
-//   const location = useLocation();
-
-//   const user = {
-//     email: "tushar@gmail.com",
-//   };
-
-//   if (user.email) {
-//     return children;
-//   }
-
-//   return <Navigate to="/login" state={{ from: location }} />;
-// };
-
-// export default PrivateRoute;
 
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const userData = useSelector((state) => state.UserData);
-  console.log(userData);
-  // const location = useLocation();
-  // const UserData = JSON.parse(localStorage.getItem("yourInfo"));
-  // console.log(UserData);
-  // if (UserData?.role == "ADMIN") {
-  //   return children;
-  // } else {
-  //   return <Navigate to="/login" />;
-  // }
-  return children;
+  const { user, loading: userloading } = useSelector(state => state.Profile)
+  if (userloading) {
+    return
+  }
+  if (user?.role == "ADMIN") {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
 };
 
 export default PrivateRoute;
