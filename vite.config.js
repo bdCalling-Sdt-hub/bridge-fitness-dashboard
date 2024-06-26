b08f472d8440e2ea06fcc470b444d15d4ed825ad
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-06-26 11:35:12 +0600

    ip && server change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.30.250",
+    host: "134.209.209.57",
     port: "3001",
   },
 });
